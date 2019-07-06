d814fa6462bbe0d04ecb028cef3caf8cc08d96fb
Fulei Nataliya <fulei.nataliya@gmail.com>
2019-07-06 20:25:16 +0300

    answers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
 package main.java;
 
 import java.util.Arrays;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        TreeSet<Integer> ts = new TreeSet<>(new MyComparator());
+        Set<Integer> ts = new TreeSet<>(new MyComparator());
 
-        ts.addAll(Arrays.asList((new Integer[]{5, 6, 8, 15, 62, 48, 1, 3, 9, 30, 7})));
+        ts.addAll(Arrays.asList(5, 6, 8, 15, 62, 48, 1, 3, 9, 30, 7));
         System.out.println(ts);
 
     }
